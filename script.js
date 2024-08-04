aebaa3e041ef1d1038a47d53e193ac7166e7b51a
ryanyoshii7 <ahouaryan3@gmail.com>
2024-08-04 04:16:03 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// scripts.js
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var navMenu = document.querySelector('header nav ul');
+    navMenu.classList.toggle('active');
+});
+
+document.getElementById('newsletter-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var email = document.getElementById('newsletter-email').value;
+    var message = document.getElementById('newsletter-message');
+
+    if (validateEmail(email)) {
+        message.textContent = 'Merci pour votre inscription!';
+        message.style.color = 'green';
+        // Envoyer le formulaire ou traiter l'inscription ici
+    } else {
+        message.textContent = 'Veuillez entrer une adresse email valide.';
+        message.style.color = 'red';
+    }
+});
+
+function validateEmail(email) {
+    var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+}
